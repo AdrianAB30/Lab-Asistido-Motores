59f093ed4a9578444673c29bc8a10ac7ddc69fb6
Adrian AB <jose.anchiraico@tecsup.edu.pe>
2024-10-07 22:26:35 -0500

    G2 Start Color Mechanic

---
+++ Assets/Scripts/ScriptableObjects/ColorData.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Color Data Enemies", menuName ="Scriptable Objects/Game 2/ColorDataEnemies")]
+public class ColorData : ScriptableObject
+{
+   public Color color;  
+}
